69274981fa5b726117eb2242ad3abf33723b0671
ota <unban5963@gmail.com>
2019-05-16 15:45:44 +0900

    add login system

--- home.php
+++ home.php
@@ -1,26 +1,79 @@
 <?php
-    $day = date(Y) . '年' . date(n) . '月' . date(j) . '日';
-    $today = date(Y).date(n).date(j);
-    $datas = fopen("./user_data/example_user.txt", 'r');
-    while($schedule_data = fgets($datas)){
-        $schedule_data = explode("/", $schedule_data);
-        if ($today === $schedule_data[0]) {
-            $today_schedule = $schedule_data;
-        }
-    }
-    array_shift($today_schedule);
+session_start();
 
-$schedule_count = count($today_schedule);
+if (!empty($_POST['log_out'])) {
+    unset($_SESSION['login_name']);
 
-$output_schedules =[];
-for ($i = 1; $i <= $schedule_count; $i += 2){
-    $time = $today_schedule[$i - 1];
-    $action = $today_schedule[$i];
-
-    $output_schedules[$time] = $action;
 }
 
-//var_dump($output_schedules);
+
+$login = 'No';
+
+if (!empty($_POST['user_pass']) && !empty($_POST['user_name'])){
+
+    $_SESSION['name'] = $_POST['user_name'];
+    $_SESSION['pass'] = $_POST['user_pass'];
+
+    $login = 'New';
+    $pass_data = fopen("./user_data/user_pass.txt", 'r');
+    while($user_info = fgets($pass_data)){
+        $user_info = explode("/", $user_info);
+        if ($_POST['user_name'] === $user_info[1] && $_POST['user_pass'] === $user_info[2]) {
+            $login = 'Yes';
+            break;
+        }
+    }
+    fclose($pass_data);
+}
+
+
+if(!empty($_POST['new'])) {
+    $pass_data = fopen("./user_data/user_pass.txt", 'r');
+    $count = 1;
+    while($user_info = fgets($pass_data)){
+        $count++; 
+    }
+    $new_user_pass_data = $count . '/' . $_SESSION['name'] . '/' . $_SESSION['pass'] . '/';
+    fclose($pass_data);
+
+    $pass_data = fopen("./user_data/user_pass.txt", 'a');
+    fwrite($pass_data, "\n" . $new_user_pass_data);
+}
+
+
+$day = date(Y) . '年' . date(n) . '月' . date(j) . '日';
+
+if ($login === 'Yes') {
+    $today = date(Y).date(n).date(j);
+    $datas = fopen("./user_data/schedule_data.txt", 'r');
+    while($schedule_data = fgets($datas)){
+        $users_data = explode(" ",$schedule_data);
+        $user_num = $users_data[0];
+        $schedule_data = explode("/", $users_data[1]);
+        if ($user_num === $user_info[0]) {
+            if ($today === $schedule_data[0]) {
+                $today_schedule = $schedule_data;
+                break;
+            }
+        }
+    }
+    $_SESSION['login_name'] = $user_info[1];
+
+    if($today_schedule){
+        array_shift($today_schedule);
+    }
+
+    $schedule_count = count($today_schedule);
+
+    $output_schedules =[];
+    for ($i = 1; $i <= $schedule_count; $i += 2){
+        $time = $today_schedule[$i - 1];
+        $action = $today_schedule[$i];
+
+        $output_schedules[$time] = $action;
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -33,20 +86,50 @@ for ($i = 1; $i <= $schedule_count; $i += 2){
     <title>Schedule</title>
 </head>
 <body>
-    <header>
-        <h1>Today Schedule</h1>
-    </header>
-    <main>
-        <h2 class="today"><?php echo $day; ?></h2>
-        <?php foreach($output_schedules as $time => $schedule): ?>
-        <div class="one_schedule">
-            <h3><?php echo $time; ?></h3>
-            <p><?php echo $schedule; ?></p>
-        </div>
-        <?php endforeach ?>
-    </main>
-    <footer>
-        <p>スケジュール管理webページ</p>
-    </footer>
+    <form action="home.php" method="post">  
+        <header>
+            <h1>Web My Schedule</h1>
+            <?php if(!empty($_SESSION['login_name'])): ?>
+            <p><?php echo $_SESSION['login_name'] ?>さん</p>
+            <?php endif ?>
+        </header>
+        <nav>
+            <?php if ($login === 'Yes'): ?>
+            <input type="submit" class="log_out" name="log_out" value="ログアウト">
+            <?php endif ?>
+        </nav>
+        <main>
+            <h2 class="today">● <?php echo $day; ?> ●</h2>
+            <?php if ($login === 'Yes'): ?>
+                <?php foreach($output_schedules as $time => $schedule): ?>
+                    <div class="one_schedule">
+                        <h3><?php echo $time; ?></h3>
+                        <p><?php echo $schedule; ?></p>
+                    </div>
+                <?php endforeach ?>
+                <?php elseif($login === 'No'): ?>
+                <div class="login">
+                    <h3>ログイン&新規登録</h3>
+                    <div class="form_textbox">
+                        <input type="text" class="user_name" name="user_name" placeholder="ニックネーム">
+                        <input type="text" class="user_pass" name="user_pass" placeholder="パスワード">
+                    </div>
+                    <input type="submit" class="log_new" name="log" value="I N">
+                </div>
+                <?php elseif($login === 'New'): ?>
+                <div class="login">
+                    <h3>ログイン&新規登録</h3>
+                    <div class="new_user_info">
+                        <h4 class="new_name">ニックネーム:<?php echo $_SESSION['name'] ?></h4>
+                        <h4 class="new_pass">パスワード:<?php echo $_SESSION['pass'] ?></h4>
+                    </div>
+                    <input type="submit" class="log_new" name="new" value="新規登録">
+                </div>
+            <?php endif ?>
+        </main>
+        <footer>
+            <p>Webスケジュール管理ページ</p>
+        </footer>
+    </form>
 </body>
 </html>
